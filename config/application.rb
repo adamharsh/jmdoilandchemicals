06447ae4dfd3a7f75d7875698ca92b04de4597ee
allinone <allinonefk@gmail.com>
2017-08-22 17:30:08 +0530

    test

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,15 @@ module Jmd2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+       config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :user_name => "railsproject100@gmail.com",
+     :password => "railsproject",
+     :authentication => :plain,
+     :enable_starttls_auto => true
+}
   end
 end
