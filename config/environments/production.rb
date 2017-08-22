cc82f63338b39940f6834768885af7e1b73ce983
allinone <allinonefk@gmail.com>
2017-08-22 17:25:32 +0530

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+   config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :user_name => "railsproject100@gmail.com",
+     :password => "railsproject",
+     :authentication => :plain,
+     :enable_starttls_auto => true
+}
 end
